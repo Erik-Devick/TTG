3786a04262351e8e1cdd400b1b6920d9711529fe
Erik-Devick <erikdevick1@gmail.com>
2024-11-06 22:53:02 -0600

    commit

--- ttg.py
+++ ttg.py
@@ -1,1 +1,16 @@
-print('TEST')
+import string
+
+negation = "~"
+conjunction = "."
+disjuction = "v"
+implies = ">"
+equality = "="
+
+test = "(A.B)vC"
+
+letters=[]
+for letter in test:
+    if letter in string.ascii_uppercase:
+        letters.append(letter)
+
+print(letters)
